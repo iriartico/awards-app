b9e891981bb8bbccbc4f38229b5e1a1b8108c0c3
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-14 10:20:55 -0400

    First views

--- awardsapp/polls/views.py
+++ awardsapp/polls/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Home Page of Awards App")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You are looking at question number {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"You are looking the result of the question number {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting for question number {question_id}")
