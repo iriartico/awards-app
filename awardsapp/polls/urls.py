3203076120597032a95c5119199a3a1e704fcfbc
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-14 11:39:37 -0400

    Removing hard coding with {%url%}

--- awardsapp/polls/urls.py
+++ awardsapp/polls/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
     # Example: /polls/
     path('', views.index, name='index'),
     # Example: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/detail/thebestappintheworld/oliverius/thebestdev', views.detail, name='detail'),
     # Example: /polls/5/results
     path('<int:question_id>/results', views.results, name='results'),
     # Example: /polls/5/vote
--- awardsapp/polls/views.py
+++ awardsapp/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
+
 def index(request):
     latest_question_list = Question.objects.all()
     return render(request, "polls/index.html", {
