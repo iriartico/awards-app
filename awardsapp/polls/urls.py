f9c45efff061f9d6e3ad5518bd16e9f0978c1e20
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-17 11:13:02 -0400

    Function vote

--- awardsapp/polls/urls.py
+++ awardsapp/polls/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     # Example: /polls/5/
     # path('<int:question_id>/', views.detail, name='detail'),
     # path('<int:question_id>/detail/thebestappintheworld/oliverius/thebestdev', views.detail, name='detail'),
-    path('<int:question_id>/detail', views.detail, name='detail'),
+    path('<int:question_id>/detail/', views.detail, name='detail'),
     # Example: /polls/5/results
-    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # Example: /polls/5/vote
-    path('<int:question_id>/vote', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
