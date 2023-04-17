eefea7f2e6733647999d0b4f2cf11e778dbbd831
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-17 11:52:57 -0400

    Implementing generic views

--- awardsapp/polls/urls.py
+++ awardsapp/polls/urls.py
@@ -6,13 +6,11 @@ app_name = "polls"
 
 urlpatterns = [
     # Example: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # Example: /polls/5/
-    # path('<int:question_id>/', views.detail, name='detail'),
-    # path('<int:question_id>/detail/thebestappintheworld/oliverius/thebestdev', views.detail, name='detail'),
-    path('<int:question_id>/detail/', views.detail, name='detail'),
+    path('<int:pk>/detail/', views.DetailView.as_view(), name='detail'),
     # Example: /polls/5/results
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
     # Example: /polls/5/vote
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
