af612adefe9ef4b17353d3b3b42fee0ebacf1113
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-19 10:11:55 -0400

    testing for class IndexView

--- awardsapp/polls/tests.py
+++ awardsapp/polls/tests.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Question
 
@@ -14,3 +15,13 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="¿Quién es el mejor Course Director de mi academia?", pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        """If not question exist, an appropiate message is displayed"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"], [])
