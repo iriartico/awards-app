7699b384b8c5f2ca16cf87a105da41c778d01ad7
Steve Jobs <sexyespadachin1@gmail.com>
2023-04-20 10:11:51 -0400

    Creating more tests for IndexView

--- awardsapp/polls/tests.py
+++ awardsapp/polls/tests.py
@@ -17,6 +17,14 @@ class QuestionModelTests(TestCase):
         self.assertIs(future_question.was_published_recently(), False)
 
 
+def create_question(question, days):
+    """Create a question with the given "question_text", and published the given numbers of the day
+       offset to now(negative for questions published in the past, positive for questions that have
+       yet to be published)"""
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question, pub_date=time)
+
+
 class QuestionIndexViewTests(TestCase):
 
     def test_no_questions(self):
@@ -25,3 +33,16 @@ class QuestionIndexViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context["latest_question_list"], [])
+
+    def test_no_future_question_are_displayed(self):
+        """If a future question is created in the database, this question isn't show until his pub_date
+           is equal to the present time"""
+        response = self.client.get(reverse("polls:index"))
+        future_question = create_question("Who is the best student?", 30)
+        self.assertNotIn(future_question, response.context["latest_question_list"])
+    
+    def test_past_question(self):
+        """Question with a pub_date in the past are displayed on the index page"""
+        past_question = create_question("Past question", -10)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(response.context["latest_question_list"], [past_question])
